bda28ebccf755273ae7cbca1050c5e187f20dc34
ysmc2018 <ysmc2018@naver.com>
2022-05-03 15:49:33 +0900

    update-hotplace

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import './App.css';
 
 function App() {
 
-  let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','남자 마스크팩 추천','아무글1','아무글2','아무글3','아무글4','아무글5','아무글6','아무글7']);
+  let [글제목, 글제목변경] = useState(['보태니컬한 인테리어 카페','초여름에 가기 좋은 루프탑','서울에서 하기 좋은 산책 코스','한강뷰 레스토랑','힙한 감성의 인테리어','수도권 산','한강 유람선 타기','둘이 먹다 하나 죽어도 모르는 맛집']);
   let [따봉, 따봉변경] = useState(0); 
 
-
+ -
   return (
     <div><div className="App">
       <div className="black-nav">
-        <div>리액트에 GA를 심어보자!!</div>
+        <div>가보고싶은 곳, 해보고싶은 것</div>
           <hr/>
       </div>
 
@@ -20,57 +20,56 @@ function App() {
 
     <div className='list'>
     <h3>{ 글제목[0] } <span onClick={() => {따봉변경(따봉+1)}}><span className='emogi'>👍</span></span>{따봉}</h3>
-        <p>2월 17일 발행</p>
+        <h4><a href="https://map.naver.com/v5/entry/place/1580543297?c=14127644.9197222,4515754.5759100,15,0,0,0,dh&placePath=%3Fentry=plt">
+          <button className='blog-button'>합정 저스티나</button></a></h4>
+          <h4><a href="https://m.place.naver.com/restaurant/1848488870/home">
+          <button className='blog-button'>홍대 발코니가든</button></a></h4>
+        <p>초록초록한 식물 인테리어로, 마음이 평안해지고 분위기도 몽글몽글해지는 나의 최애 카페</p>
         <hr/>
     </div>
 
     <div className='list'>
-      <h3><a href="https://map.naver.com/v5/search/%EA%B0%95%EB%82%A8%EC%9A%B0%EB%8F%99/place/1820911105?placePath=%3Fentry=pll%26from=nx%26fromNxList=true&c=14139355.2737438,4511430.4814907,15,0,0,0,dh">
+      <h3><a href="https://map.naver.com/v5/search/oktop/place/658074326?placePath=%3Fentry=pll%26from=nx%26fromNxList=true&c=14135261.6441852,4513905.6440083,15,0,0,0,dh">
         <button className='blog-button'>{글제목[1]}</button></a></h3>
-        <p>2월 15일 발행</p>
+        <p>남산이 바로 눈앞에 보이는 야경맛집 루프탑! 이집 야경 잘하네!</p>
         <hr/>
     </div>
 
     <div className='list'>
-        <h3>{글제목[2]}</h3>
-        <p>2월 14일 발행</p>
+        <h3>{글제목[2]}</h3>        
         <hr/>
     </div>
 
     <div className='list'>
         <h3>{글제목[3]}</h3>
-        <p>2월 14일 발행</p>
         <hr/>
     </div>
     
     <div className='list'>
         <h3>{글제목[4]}</h3>
-        <p>2월 14일 발행</p>
         <hr/>
     </div>
     
     <div className='list'>
         <h3>{글제목[5]}</h3>
-        <p>2월 14일 발행</p>
         <hr/>
     </div>
     
     <div className='list'>
         <h3>{글제목[6]}</h3>
-        <p>2월 14일 발행</p>
+        <p>sdfsdf</p>
         <hr/>
     </div>
     
     <div className='list'>
         <h3>{글제목[7]}</h3>
-        <p>2월 14일 발행</p>
         <hr/>
     </div>
     
     <div className='modal'>
-      <h2>제목</h2>
-      <p>날짜</p>
-      <p>상세내용</p>
+      <h2>아직 갈 곳이 많다</h2>
+      <p>요즘 날씨 정말 좋던데</p>
+      <p>5월이 이렇게 좋은 적은 처음이야!</p>
     </div>
     </div>
   );
