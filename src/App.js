d418f5d1b9e1c2a3558d36239efecc5cd9f60623
ysmc2018 <ysmc2018@naver.com>
2022-05-09 10:33:47 +0900

    ag

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
 
-  let [글제목, 글제목변경] = useState(['보태니컬한 인테리어 카페','초여름에 가기 좋은 루프탑','서울에서 하기 좋은 산책 코스','한강뷰 레스토랑','힙한 감성의 인테리어','수도권 산','한강 유람선 타기','둘이 먹다 하나 죽어도 모르는 맛집']);
+  let [글제목, 글제목변경] = useState(['보태니컬한 인테리어 카페','초여름에 가기 좋은 루프탑','서울에서 하기 좋은 산책 코스','한강뷰 레스토랑','힙한 감성의 인테리어','수도권 산','한강 유람선 타기','서울 맛집']);
   let [따봉, 따봉변경] = useState(0); 
 
  
@@ -31,7 +31,7 @@ function App() {
     <div className='list'>
       <h3><a href="https://map.naver.com/v5/search/oktop/place/658074326?placePath=%3Fentry=pll%26from=nx%26fromNxList=true&c=14135261.6441852,4513905.6440083,15,0,0,0,dh" onClick="ga('send','event','어떤걸봤나','버튼클릭');">
         <button className='blog-button'>{글제목[1]}</button></a></h3>
-        <p>남산타워가 눈앞에 보이는 야경맛집 루프탑! 이집 야경 잘하네!</p>
+        <p>남산타워가 눈앞에 보이는 야경맛집 루프탑</p>
         <hr/>
     </div>
 
@@ -68,7 +68,6 @@ function App() {
     <div className='modal'>
       <h2>아직 갈 곳이 많다</h2>
       <p>요즘 날씨 정말 좋던데</p>
-      <p>5월이 이렇게 좋은 적은 처음이야!</p>
     </div>
     </div>
   );
